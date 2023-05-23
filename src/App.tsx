f7959440d4caafe74a180be87db372d7067eecf0
v <>
2023-05-23 12:08:44 +0200

    better layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './pages/landing';
 import About from './pages/about';
+import Footer from './pages/footer';
 
 const App = () => {
   return (
     <div className="App background-img">
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/about" element={<About />} />
-        </Routes>
-      </BrowserRouter>
+      <div className="content">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Landing />} />
+            <Route path="/about" element={<About />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
+      <Footer />
     </div>
   );
 };
